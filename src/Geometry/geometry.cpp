0eeaae338da963147d40a1867f2d41f93540fc80
pan-kuleczka-remote <jeremijjsenior@gmail.com>
2021-11-30 08:08:33 +0100

    Geometry vector angleTo() function More strict const modifiers

--- src/Geometry/geometry.cpp
+++ src/Geometry/geometry.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 
-template <class _derivedType, unsigned int _dimension, class _T = long double>
+template <class _derivedType, std::size_t _dimension, class _T = long double>
 struct VectorBase
 {
     std::array<_T, _dimension> coordinates;
 
     template <class __derivedType>
-    _derivedType operator+(const VectorBase<__derivedType, _dimension, _T> &other)
+    _derivedType operator+(const VectorBase<__derivedType, _dimension, _T> &other) const
     {
         _derivedType sumVector = _derivedType();
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             sumVector.coordinates[i] = this->coordinates[i] + other.coordinates[i];
         return sumVector;
     }
@@ -22,10 +22,10 @@ struct VectorBase
     }
 
     template <class __derivedType>
-    _derivedType operator-(const VectorBase<__derivedType, _dimension, _T> &other)
+    _derivedType operator-(const VectorBase<__derivedType, _dimension, _T> &other) const
     {
         _derivedType diffVector = _derivedType();
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             diffVector.coordinates[i] = this->coordinates[i] - other.coordinates[i];
         return diffVector;
     }
@@ -37,79 +37,85 @@ struct VectorBase
         return *this;
     }
 
-    _derivedType operator*(_T x)
+    _derivedType operator*(const _T &x) const
     {
         _derivedType prodVector = _derivedType();
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             prodVector.coordinates[i] = this->coordinates[i] * x;
         return prodVector;
     }
 
-    _derivedType &operator*=(_T x)
+    _derivedType &operator*=(const _T &x)
     {
         *this = *this * x;
         return *this;
     }
 
-    _derivedType operator/(_T x)
+    _derivedType operator/(const _T &x) const
     {
         if (x == _T(0))
             throw std::runtime_error("VectorBase math error: division by 0");
 
         _derivedType prodVector = _derivedType();
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             prodVector.coordinates[i] = this->coordinates[i] / x;
         return prodVector;
     }
 
-    _derivedType &operator/=(_T x)
+    _derivedType &operator/=(const _T &x)
     {
         *this = *this / x;
         return *this;
     }
 
     template <class __derivedType>
-    _T dotProduct(const VectorBase<__derivedType, _dimension, _T> &other)
+    _T dotProduct(const VectorBase<__derivedType, _dimension, _T> &other) const
     {
         _T sum = _T(0);
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             sum += this->coordinates[i] * other.coordinates[i];
         return sum;
     }
 
     template <class __derivedType>
-    _T operator*(const VectorBase<__derivedType, _dimension, _T> &other) { return dotProduct(other); }
+    _T operator*(const VectorBase<__derivedType, _dimension, _T> &other) const { return dotProduct(other); }
 
-    _T lengthSquare()
+    _T lengthSquare() const
     {
         _T result = _T(0);
-        for (unsigned int i = 0; i < _dimension; ++i)
+        for (std::size_t i = 0; i < _dimension; ++i)
             result += this->coordinates[i] * this->coordinates[i];
         return result;
     }
 
-    _T length()
+    _T length() const
     {
-        return sqrt(this->lengthSquare());
+        return sqrtl(this->lengthSquare());
     }
 
-    _derivedType normalized()
+    _derivedType normalized() const
     {
         return *this / this->length();
     }
+
+    template <class __derivedType>
+    long double angleTo(const VectorBase<__derivedType, _dimension, _T> &other) const
+    {
+        return acosl(this->normalized().dotProduct(other.normalized()));
+    }
 };
 
-template <class _derivedType, unsigned int _dimension, class _T = long double>
+template <class _derivedType, std::size_t _dimension, class _T = long double>
 _T abs(VectorBase<_derivedType, _dimension, _T> vec)
 {
-    return sqrt(vec.lengthSquare());
+    return sqrtl(vec.lengthSquare());
 }
 
-template <class _derivedType, unsigned int _dimension, class _T = long double>
-std::ostream &operator<<(std::ostream &stream, VectorBase<_derivedType, _dimension, _T> vec)
+template <class _derivedType, std::size_t _dimension, class _T = long double>
+std::ostream &operator<<(std::ostream &stream, const VectorBase<_derivedType, _dimension, _T> &vec)
 {
     stream << '(';
-    for (unsigned int i = 0; i < _dimension - 1; ++i)
+    for (std::size_t i = 0; i < _dimension - 1; ++i)
         stream << vec.coordinates[i] << ", ";
     if (_dimension > 0)
         stream << vec.coordinates[_dimension - 1];
@@ -121,17 +127,17 @@ template <class _T = long double>
 struct Vector2 : VectorBase<Vector2<_T>, 2, _T>
 {
     using VectorBase<Vector2<_T>, 2, _T>::coordinates;
-    const _T &x = coordinates[0];
-    const _T &y = coordinates[1];
+    _T &x = coordinates[0];
+    _T &y = coordinates[1];
 };
 
 template <class _T = long double>
 struct Vector3 : VectorBase<Vector3<_T>, 3, _T>
 {
     using VectorBase<Vector3<_T>, 3, _T>::coordinates;
-    const _T &x = coordinates[0];
-    const _T &y = coordinates[1];
-    const _T &z = coordinates[2];
+    _T &x = coordinates[0];
+    _T &y = coordinates[1];
+    _T &z = coordinates[2];
 
     Vector3 crossProduct(const Vector3 &other)
     {
@@ -145,14 +151,15 @@ template <class _T = long double>
 struct Vector4 : VectorBase<Vector4<_T>, 4, _T>
 {
     using VectorBase<Vector4<_T>, 4, _T>::coordinates;
-    const _T &x = coordinates[0];
-    const _T &y = coordinates[1];
-    const _T &z = coordinates[2];
-    const _T &w = coordinates[3];
+    _T &x = coordinates[0];
+    _T &y = coordinates[1];
+    _T &z = coordinates[2];
+    _T &w = coordinates[3];
 };
 
 int main()
 {
-    Vector4 v({1, 1, 1});
-    std::cout << v.normalized().x << " " << v.normalized().length() << "\n";
+    Vector2 v({0, 1});
+    Vector2 w({1, 0});
+    std::cout << v.angleTo(w) << "\n";
 }
